f5965b9272f46139c40713871f7f64b288f7c504
abc80483 <abc80483@gmail.com>
2020-10-07 18:49:16 +0800

    segment test

--- mfccextract.py
+++ mfccextract.py
@@ -1,16 +1,29 @@
 import librosa
+from librosa import display
 import matplotlib.pyplot as plt
+import numpy as np
 
 y, sr = librosa.load('C:/sound/XC62823.wav')
-#plot
-plt.figure()
-plt.plot(y)
-plt.title("test")
-plt.show()
 
+print(y.shape)
+segmentation = 100000
+
+for i in range(1, len(y)):
+    if(i%segmentation==0):
+        newy = y[i-segmentation:i]
+        newy = librosa.feature.mfcc(newy)
+        plt.figure()
+        librosa.display.specshow(newy, sr=sr)
+        plt.tight_layout()
+
+"""        
 y = librosa.feature.mfcc(y)
+np.swapaxes(y, 1, 0)
+librosa.display.specshow(y, sr=sr, )
 
+plt.colorbar(format='%+2.0f dB')
 plt.figure()
 plt.plot(y)
 plt.title("mfcc")
 plt.show()
+"""

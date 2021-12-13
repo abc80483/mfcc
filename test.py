3d64952b32aa741ce6fb52a25347c6143f24b31d
abc80483 <abc80483@gmail.com>
2021-12-13 16:24:20 +0800

    deleted useless files

--- test.py
+++
@@ -1,7 +0,0 @@
-from pydub import AudioSegment
-
-newAudio = AudioSegment.from_wav("XC62823.wav")
-for i in range(1, 1000000, 200):
-    newAudio = newAudio[i:i+10000]
-    exportstr = "newsong" + str(i) + ".wav"
-    newAudio.export(exportstr, format="wav") #Exports to a wav file in the current path.
